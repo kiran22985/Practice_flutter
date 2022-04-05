b87d3a88645d27b6066f55aa26a52d4e8b5d9962
Kiran kumari Giri <59924309+kiran22985@users.noreply.github.com>
2022-04-05 13:21:18 +0545

    days4 completed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
+
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: 
-      Center(
-        child: Text('Login Here',
-        textAlign: TextAlign.center,
-        textDirection: TextDirection.rtl,
-        //textScaleFactor: 3.0,
-        style: TextStyle(
-          decoration: TextDecoration.overline,
-          color: Colors.blue,
-          fontSize:30,
-          fontWeight: FontWeight.bold,
-          fontStyle: FontStyle.italic,
-        ),),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+              Image.asset('assets/images/login_image.png',fit: BoxFit.cover,),
+              const Text('Welcome',style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),),
+              const SizedBox(height:20 ,),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration:const InputDecoration(
+                        hintText: "Enter ur username",
+                        labelText: "Username",
+                        
+                      ),
+                    ),
+                    TextFormField(
+                      //maxLines: 5,
+                      obscureText: true,
+                      decoration:const InputDecoration(
+                        hintText: "Enter ur password",
+                        labelText: "Password",
+                        
+                      ),
+                    ),
+                    SizedBox(height:20),
+                    ElevatedButton(onPressed: (){}, child: const Text('Login',),style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.white,//foreground
+                      primary: Colors.amber//background
+                    ),)
+                  ],
+                ),
+              )
+          ],
+        ),
       )),
     );
   }
